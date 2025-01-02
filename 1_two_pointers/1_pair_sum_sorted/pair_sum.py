e47fa88b8a122c2d280eaa8247dd53358f855ca9
Erik Mercado <mercadoeirk1031@gmail.com>
2025-01-01 16:20:19 -0800

    Formatted files & removed pytest import

--- 1_two_pointers/1_pair_sum_sorted/pair_sum.py
+++ 1_two_pointers/1_pair_sum_sorted/pair_sum.py
@@ -1,11 +1,12 @@
-'''
+"""
 Given an array of integers sorted in ascending order and a target value, return the indexes
 of any pair of numbers in the array that sum to the target.
 
 The order of the indexes in the result doesn't matter. If you pair is found return an empty array.
-'''
+"""
 
 from typing import List
+
 
 def pair_sum(nums: List[int], target: int) -> List[int]:
     left, right = 0, len(nums) - 1
@@ -14,13 +15,14 @@ def pair_sum(nums: List[int], target: int) -> List[int]:
         curr_sum = nums[left] + nums[right]
         if curr_sum == target:
             return [left, right]
-        
+
         elif curr_sum > target:
             right -= 1
-        
+
         else:
             left += 1
-            
+
     return []
+
 
 # Time Complexity: O(n), Space Complexity O(1)
--- 1_two_pointers/1_pair_sum_sorted/test_pair_sum.py
+++ 1_two_pointers/1_pair_sum_sorted/test_pair_sum.py
@@ -1,5 +1,5 @@
-import pytest
 from pair_sum import pair_sum
+
 
 def test_pair_sum_empty_array() -> None:
     nums = []
@@ -7,11 +7,13 @@ def test_pair_sum_empty_array() -> None:
     result = pair_sum(nums, target)
     assert result == []
 
+
 def test_pair_sum_one_element() -> None:
     nums = [1]
     target = 1
     result = pair_sum(nums, target)
     assert result == []
+
 
 def test_pair_sum_two_element_contains_pair() -> None:
     nums = [2, 3]
@@ -19,11 +21,13 @@ def test_pair_sum_two_element_contains_pair() -> None:
     result = pair_sum(nums, target)
     assert result == [0, 1]
 
+
 def test_pair_sum_two_element_no_pair() -> None:
     nums = [2, 4]
     target = 5
     result = pair_sum(nums, target)
     assert result == []
+
 
 def test_pair_sum_duplicate_values() -> None:
     nums = [2, 2, 3]
@@ -31,11 +35,13 @@ def test_pair_sum_duplicate_values() -> None:
     result = pair_sum(nums, target)
     assert result == [0, 2] or result == [1, 2]
 
+
 def test_pair_sum_with_negative() -> None:
     nums = [-1, 2, 3]
     target = 2
     result = pair_sum(nums, target)
     assert result == [0, 2]
+
 
 def test_pair_sum_both_numbers_of_target_negative() -> None:
     nums = [-3, -2, -1]
@@ -43,17 +49,20 @@ def test_pair_sum_both_numbers_of_target_negative() -> None:
     result = pair_sum(nums, target)
     assert result == [0, 1]
 
+
 def test_pair_sum_multiple_pairs() -> None:
     nums = [1, 2, 3, 4, 5, 6]
     target = 7
     result = pair_sum(nums, target)
     assert result == [1, 4] or result == [0, 5]
 
+
 def test_pair_sum_large_array() -> None:
-    nums = list(range(1, 10**6))
+    nums = list(range(1, 10 ** 6))
     target = 1999997
     result = pair_sum(nums, target)
     assert result == [999997, 999998]
+
 
 def test_pair_sum_only_negative_numbers() -> None:
     nums = [-5, -4, -3, -2, -1]
@@ -61,11 +70,13 @@ def test_pair_sum_only_negative_numbers() -> None:
     result = pair_sum(nums, target)
     assert result == [0, 4]
 
+
 def test_pair_sum_positive_array_negative_target() -> None:
     nums = [1, 2, 3, 4, 5]
     target = -1
     result = pair_sum(nums, target)
     assert result == []
+
 
 def test_pair_sum_equal_values() -> None:
     nums = [3, 3]
@@ -73,11 +84,13 @@ def test_pair_sum_equal_values() -> None:
     result = pair_sum(nums, target)
     assert result == [0, 1]
 
+
 def test_pair_sum_target_zero() -> None:
     nums = [-3, -2, 0, 2, 3]
     target = 0
     result = pair_sum(nums, target)
     assert result == [0, 4] or result == [1, 3]
+
 
 def test_pair_sum_large_target() -> None:
     nums = [1, 2, 3, 4, 5]
