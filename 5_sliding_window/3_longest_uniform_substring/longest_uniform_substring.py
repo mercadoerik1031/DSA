3643927c7364363ebe9824e017c0fed566273010
Erik Mercado <mercadoeirk1031@gmail.com>
2025-01-11 15:45:37 -0800

    Set up 3. Longest Uniform Substring

---
+++ 5_sliding_window/3_longest_uniform_substring/longest_uniform_substring.py
@@ -0,0 +1,13 @@
+'''
+A Uniform substring is one in which all character are identical.
+Given a string, determine the length of the longest uniform substring that can be formed by replacing 
+up to k characters
+
+Example:
+    Input: s = "aabcdcca", k = 2
+    Output: 5
+
+Explanation:
+    If we can only replace 2 characters, the longest uniform substring we can achieve is "ccccc",
+    obtained by replace b and d with c
+'''
